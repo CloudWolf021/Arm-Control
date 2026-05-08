Implemented gradient descent interventions for helping avoid singularities. Fixed issues with poaition unreachability logic. Performed other small logic improvements.

Helpers.py
@@ -70,6 +70,24 @@ def MoveToJointPositionsRaw(data, input, requireValid, isFirstArm = True) -> boo
 # ###########################################################################################
 
 '''
+Move the first arm back to its initial position
+
+Note: this is called when there is is an unreachable position or timeout for reaching the final
+position. 
+'''
+
+def Reset(data, model, viewer):
+    for i in range(Vars.DOF):
+        data.ctrl[i] = 0
+    data.ctrl[3] = -0.15
+
+    for i in range(Vars.MODEL_STEP_ITERS):
+        mujoco.mj_step(model, data)
+    viewer.sync()
+
+# ###########################################################################################
+
+'''
 Determine if either the first or second arm has reached the desired target position, using a 
 specific threshold. This function takes global (x, y, z) coordinates, and obtains 
 instantaneous global coordinates for the end effector, which are used for determining error. 
@@ -128,9 +146,13 @@ def TruncateJacobian(jacobian, startIndex):
 '''
 Using TruncateJacobian, extract the relevant 3 by Vars.DOF matrix for the first arm.
 '''
-def ExtractFirstJacobian(model, data):
-    jacNeeded1 = np.zeros((3, Vars.ADJ_DOF))
-    jacOther1 = np.zeros((3, Vars.ADJ_DOF))
+def ExtractFirstJacobian(model, data, isLargeMatrix = True):
+    numCols = Vars.ADJ_DOF
+    if not(isLargeMatrix):
+        numCols = Vars.DOF
+
+    jacNeeded1 = np.zeros((3, numCols))
+    jacOther1 = np.zeros((3, numCols))
     mujoco.mj_jacSite(model, data, jacNeeded1, jacOther1, 0)
 
     # Extract the relevant portion of the jacobian
@@ -150,3 +172,4 @@ def ExtractSecondJacobian(model, data):
     return TruncateJacobian(jacNeeded2, Vars.DOF)
 
 # END Jacobian Helpers
+

JacobianGradSolve.py
@@ -3,9 +3,10 @@ import numpy as np
 import math
 
 import Vars
+import MatrixHelper
 
 '''
-A module for solving the inverse kinematics problem using gradient descent. 
+A module for solving the inverse kinematics problem using gradient descent.
 '''
 
 
@@ -32,10 +33,9 @@ The optimization criteria is the squared error from the desired joint (x, y, z) 
 By using a default of all zeroes for init, we effectively resume from the previous location with 
 zero joint position changes. 
 '''
-def GetSingleSolutionJacobian(jacobian, desiredXYZ, init = [0, 0, 0, 0, 0, 0, 0]):
+def GetSingleSolutionJacobian(jacobian, desiredXYZ, learningRate = 0.89, init = [0, 0, 0, 0, 0, 0, 0]):
     # Reference on list comprehension syntax: https://www.w3schools.com/python/python_lists_comprehension.asp
     cur = [init[i] for i in range(Vars.DOF)]
-    learningRate = 0.89
     numSteps = 60
     for i in range(numSteps):
         # Current computed changes
@@ -59,7 +59,42 @@ def GetSingleSolutionJacobian(jacobian, desiredXYZ, init = [0, 0, 0, 0, 0, 0, 0]
 # ###########################################################################################
 
 '''
-Get a new update for the joint positions based on the leas-squares solution found by gradient
+A modified gradient descent-based algorithm that left multiplies the standard equation by 
+the transpose of the Jacobian on both sides, and performs an adjustment to attempt to avoid
+singularities. 
+
+Note: # Inspired by the Levenberg-Marquardt Algorithm: https://en.wikipedia.org/wiki/Levenberg%E2%80%93Marquardt_algorithm
+'''
+
+def GetSingleSolutionJacobianSpecial(jacobian, desiredXYZ, init = [0, 0, 0, 0, 0, 0, 0]):
+    # Let the jacobian be J, and dj be the joint position changes, and dx be the overall position change
+    # J*d=dx -> (J^T*J)*dj = J^T*dx
+    # Since J^T*J is a square matrix, we can adjust it
+    transpose = np.transpose(jacobian)
+    raw = transpose @ jacobian
+
+    adjusted = MatrixHelper.AdjustMatrix(raw)
+    rhs = transpose @ desiredXYZ
+    # Now solving adjusted*dj = rhs
+    learningRate = 1.6
+    cur = [init[i] for i in range(Vars.DOF)]
+    numSteps = 150
+    for i in range(numSteps):
+        curLhs = adjusted @ np.array(cur)
+        # For each joint
+        for j in range(Vars.DOF):
+            totalGrad = 0
+            # Sum gradient for each output
+            for k in range(Vars.DOF):
+                totalGrad += 2 * (curLhs[k] - rhs[k]) * adjusted[j][k]
+            #print(totalGrad)
+            cur[j] -= totalGrad * learningRate
+    return np.array(cur)
+
+# ###########################################################################################
+
+'''
+Get a new update for the joint positions based on the least-squares solution found by gradient
 descent.
 
 Note: the solution obtained may not actually lead to the desired (x, y, z) position since the
@@ -67,15 +102,18 @@ Jacobian is not constant, and this method is used for just a single step of the 
 
 Note: Logic here is slightly different than for than for the other solvers so some code is 
 duplicated here for simplicity. 
-'''
-def GetRawJointPositionListJacobianSolve(data, model, x, y, z, jacobian, isFirstArm = True):
-    prevSq = Vars.SSQ_ERROR
 
+Note: this method supports standard gradient descent, gradient descent with learning rate adjustment, 
+and special gradient descent using a modified matrix.
+'''
+def GetRawJointPositionListJacobianSolve(data, model, x, y, z, jacobian, handleSingularity, adjustMatrix, isFirstArm = True):
+    prevSq = Vars.SSQ_ERROR
+    learningRate = 0.89
     index = 0
     if not(isFirstArm):
         # First arm
         index = 1
-    initX = data.site_xpos[index][0]
+    initX = data.site_xpos[index][0] 
     initY = data.site_xpos[index][1]
     initZ = data.site_xpos[index][2]
 
@@ -88,15 +126,25 @@ def GetRawJointPositionListJacobianSolve(data, model, x, y, z, jacobian, isFirst
     deltaErr = prevSq - sq 
 
     # Track continuous lack of improvements
-    if (deltaErr < Vars.IMPROVEMENT_FAIL_THRESHOLD and deltaErr > 0 and Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD):
+    # Instead of assessing raw change in error, it is assessed relative to the current error
+    # Vars.IMPROVEMENT_FAIL_THRESHOLD/Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD is used as an approximate proportional threshold
+    if (deltaErr/sq < Vars.IMPROVEMENT_FAIL_THRESHOLD/Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD and deltaErr > 0 and sq > Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD and prevSq > 0):
         Vars.CUR_FAIL_ITERS += 1
+        if handleSingularity:
+            learningRate = 1.8
     else:
        Vars.CUR_FAIL_ITERS = 0 
+       if handleSingularity:
+        # revert
+            learningRate = 0.89
 
     param = 1
-    # Get delta update
-    #res = param*(trans @ np.array([dx, dy, dz]))
-    res = param*GetSingleSolutionJacobian(jacobian, np.array([dx, dy, dz]))
+    # Get delta update in res
+    if adjustMatrix:
+        res = param*GetSingleSolutionJacobianSpecial(jacobian, np.array([dx, dy, dz]))
+    else:
+        # Standard
+        res = param*GetSingleSolutionJacobian(jacobian, np.array([dx, dy, dz]), learningRate)
 
     offset = 0
     if not(isFirstArm):

JacobianIterSolve.py
@@ -71,16 +71,16 @@ def GetRawJointPositionListHelper(data, model, x, y, z, jacobian, approxMatrix, 
     sq = dx**2+dy**2+dz**2
     Vars.SSQ_ERROR = sq
     deltaErr = prevSq - sq 
+    mustCorrect = False
 
     # Track continuous lack of improvements
-    if (deltaErr < Vars.IMPROVEMENT_FAIL_THRESHOLD and deltaErr > 0 and sq > Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD):
+    if (deltaErr/sq < Vars.IMPROVEMENT_FAIL_THRESHOLD/Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD and deltaErr > 0 and sq > Vars.CHECK_IMPROVEMENT_FAIL_THRESHOLD and prevSq > 0):  
         Vars.CUR_FAIL_ITERS += 1
     else:
        Vars.CUR_FAIL_ITERS = 0 
 
     # Compute multiplier depending on squared error
-    param = GetParam(sq, solverType == Vars.JT)    
-
+    param = GetParam(sq, solverType == Vars.JT) 
     res = param*(approxMatrix @ np.array([dx, dy, dz]))
     
     offset = 0

MatrixHelper.py
@@ -9,6 +9,21 @@ Matrix operation helper module for getting a safe pseudoinverse
 DET_THRESHOLD = 0.0000001
 K_ADJ = 0.0001  # Will add K_ADJ to each eigenvalue
 
+
+'''
+Adjust a square matrix so that its absolute determinant exceeds some threshold to attempt to
+yield a better-behaved matrix. 
+'''
+def AdjustMatrix(matrixIn):
+    while (math.fabs(np.linalg.det(matrixIn)) < DET_THRESHOLD):
+        # Iteratively apply correction
+        # Adjust a matrix by adding K_ADJ to each element along the main diagonal
+        for i in range(len(matrixIn)):
+            matrixIn[i][i] += K_ADJ
+    return matrixIn
+
+# ###########################################################################################
+
 '''
 Get a safe, adjusted pseudoinverse that will account for singularities. 
 This function will use a correction term in the case that there is a potential 
@@ -21,13 +36,8 @@ def GetAdjustedPInv(matrixIn):
     transpose = np.transpose(matrixIn)
     raw = transpose @ matrixIn
 
-    # Then, check determinant to see if it is invertible. 
-    while (math.fabs(np.linalg.det(raw)) < DET_THRESHOLD):
-        # Iteratively apply correction
-        # Adjust a matrix by adding K_ADJ to each element along the main diagonal
-        for i in range(len(raw)):
-            raw[i][i] += K_ADJ
-    
-    # Now, this matrix should be safely invertible
-    inv = np.linalg.inv(raw)
+    adjusted = AdjustMatrix(raw)
+    # Now, this matrix should be safely invertible and better behaved
+
+    inv = np.linalg.inv(adjusted)
     return inv @ transpose